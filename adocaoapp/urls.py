6bb38bf72afb1f6f52cb3a6fda5d895f37c34aaf
Cauanmguerreiro <cauanguerreiro@hotmail.com>
2025-06-19 14:40:14 -0300

    Alterações

--- adocaoapp/urls.py
+++ adocaoapp/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from django.urls import path
-from django.urls import path, include # include já estava aqui
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- apps/socialnetworks/models.py
+++ apps/socialnetworks/models.py
@@ -3,7 +3,9 @@ from django.db import models
 # Create your models here.
 class Socialnetwork(models.Model):
     name = models.CharField('Nome', max_length=50)
-    description = models.TextField('Descricao', max_length=100)
+    description = models.TextField('Descrição', max_length=100, blank=True, null=True) # Corrigido 'Descricao' e tornado opcional
+    url_prefix = models.CharField('Prefixo da URL (ex: https://twitter.com/)', max_length=200, blank=True, null=True)
+    icon_class = models.CharField('Classe do Ícone (ex: fab fa-twitter)', max_length=50, blank=True, null=True) # Para ícones em um frontend
     
     class Meta:
         verbose_name = 'Rede Social'
@@ -12,4 +14,3 @@ class Socialnetwork(models.Model):
 
     def __str__(self):
         return self.name
-
